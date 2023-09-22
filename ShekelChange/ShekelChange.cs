ac346866c76ce268edbbd9e7a272c3ae4828fc95
Maorku <indalelso@gmail.com>
2023-09-22 03:48:57 +0300

    small fix

--- ShekelChange/ShekelChange.cs
+++ ShekelChange/ShekelChange.cs
@@ -10,29 +10,29 @@ namespace ShekelChange
     {
         static void Main(string[] args)
         {
-            static void Main(string[] args)
-            {
+           
+            
+         //input
+         Console.WriteLine("Enter amount of change:");
+         int change = int.Parse(Console.ReadLine());
 
-                //input
-                Console.WriteLine("Enter amount of change:");
-                int change = int.Parse(Console.ReadLine());
+         //calc
+         int tenShekelCoins = change / 10;
+         change %= 10;
 
-                //calc
-                int tenShekelCoins = change / 10;
-                change %= 10;
+         int fiveShekelCoins = change / 5;
+         change %= 5;
 
-                int fiveShekelCoins = change / 5;
-                change %= 5;
+         int oneShekelCoins = change;
 
-                int oneShekelCoins = change;
+         int totalCoins = tenShekelCoins + fiveShekelCoins + oneShekelCoins;
 
-                int totalCoins = tenShekelCoins + fiveShekelCoins + oneShekelCoins;
-
-                //output
-                Console.WriteLine("Number of 10₪ coins: " + tenShekelCoins);
-                Console.WriteLine("Number of 5₪ coins: " + fiveShekelCoins);
-                Console.WriteLine("Number of 1₪ Shekel coins: " + oneShekelCoins);
-                Console.WriteLine("Total number of coins: " + totalCoins);
-            }
+         //output
+         Console.WriteLine("Number of 10 Shekel coins: " + tenShekelCoins);
+         Console.WriteLine("Number of 5 Shekel coins: " + fiveShekelCoins);
+         Console.WriteLine("Number of 1 Shekel coins: " + oneShekelCoins);
+         Console.WriteLine("Total number of coins: " + totalCoins);
+            
+        }
     }
-}
+}   
