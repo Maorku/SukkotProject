bceea0434ad5d8e4ad41e6874fba230a1089144d
Maorku <indalelso@gmail.com>
2023-09-23 03:01:20 +0300

    +Finished DinaCS

--- DinaCS/DinaCS.cs
+++ DinaCS/DinaCS.cs
@@ -10,7 +10,33 @@ namespace DinaCS
     {
         static void Main(string[] args)
         {
+            // דינה מעוניינת לדעת את הציון הכולל שלה במדעי המחשב. ידוע כי שתי היחידות
+            //הראשונות מהוות % 33 מהציון, היחידה השלישית מהווה % 17 מהציון, ושתי
+            //היחידות האחרונות מהוות % 50 מהציון הכללי. פתחו אלגוריתם המגריל את שלושת
+            //ציוניה של דינה במדעי המחשב(בשתי היחידות הראשונות, ביחידה השלישית ובשתי
+            //היחידות האחרונות) כמספרים שלמים בין - 0 ל - 100 ומציג כפלט את ציונה הכולל.
 
+            double grade33, grade17, grade50, totalgrd;
+            Random rnd = new Random();
+
+            grade17 = rnd.Next(0, 101);
+            grade33 = rnd.Next(0, 101);
+            grade50 = rnd.Next(0, 101);
+
+            grade17 = grade17 * 0.17;
+            grade33 = grade33 * 0.33;
+            grade50 = grade50 * 0.50;
+
+            Console.WriteLine("Dina's grades are");
+            Console.WriteLine($"Unit 1 and 2: {grade33}");
+            Console.WriteLine($"Unit 3: {grade17}");
+            Console.WriteLine($"Unit 4 and 5: {grade50}");
+
+            totalgrd = grade17 + grade33 + grade50;
+
+            Console.WriteLine($"Dina's Computer Science grade is: {totalgrd}");
+            
+            
         }
     }
 }
